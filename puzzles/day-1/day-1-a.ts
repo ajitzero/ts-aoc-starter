d66c48c2b87b1d4a8cfed11f7d530391c8e990f2
Ajit <ajitzero@gmail.com>
2023-12-03 05:32:55 +0530

    Done with Day 1

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -2,8 +2,29 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day1a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const codes = await readData(dataPath);
+  let sum = 0;
+  for (const code of codes) {
+    const letters = code.split('');
+    let [first, last] = [0, 0];
+    for (const letter of letters) {
+      const num = parseInt(letter);
+      if (!Number.isNaN(num)) {
+        first = num;
+        break;
+      }
+    }
+    for (let i = letters.length - 1; i >= 0; --i) {
+      const num = parseInt(letters[i]);
+      if (!Number.isNaN(num)) {
+        last = num;
+        break;
+      }
+    }
+    const current = first * 10 + last;
+    sum += current;
+  }
+  return sum;
 }
 
 const answer = await day1a();
--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -1,9 +1,66 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const patternsValue = new Map<string, number>([
+  ['1', 1],
+  ['2', 2],
+  ['3', 3],
+  ['4', 4],
+  ['5', 5],
+  ['6', 6],
+  ['7', 7],
+  ['8', 8],
+  ['9', 9],
+  ['one', 1],
+  ['two', 2],
+  ['six', 6],
+  ['four', 4],
+  ['five', 5],
+  ['nine', 9],
+  ['three', 3],
+  ['seven', 7],
+  ['eight', 8],
+]);
+
+const NUMBER_LENGTHS = [1, 3, 4, 5];
+const getIfValid = (index: number, code: string): number | null => {
+  const CODE_LENGTH = code.length;
+  for (const LENGTH of NUMBER_LENGTHS) {
+    if (
+      index + LENGTH <= CODE_LENGTH &&
+      patternsValue.has(code.slice(index, index + LENGTH))
+    ) {
+      return patternsValue.get(code.slice(index, index + LENGTH));
+    }
+  }
+  return null;
+};
+
 export async function day1b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const codes = await readData(dataPath);
+  let sum = 0;
+  for (const code of codes) {
+    const CODE_LENGTH = code.length;
+    let [first, last] = [0, 0];
+
+    for (let i = 0; i < CODE_LENGTH; ++i) {
+      const res = getIfValid(i, code);
+      if (res) {
+        first = res;
+        break;
+      }
+    }
+    for (let i = CODE_LENGTH - 1; i >= 0; --i) {
+      const res = getIfValid(i, code);
+      if (res) {
+        last = res;
+        break;
+      }
+    }
+    const current = first * 10 + last;
+    sum += current;
+  }
+  return sum;
 }
 
 const answer = await day1b();

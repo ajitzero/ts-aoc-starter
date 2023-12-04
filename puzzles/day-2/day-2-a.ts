00298d237ca969defe27926a65405bf38c334e99
Ajit <ajitzero@gmail.com>
2023-12-04 05:58:34 +0530

    Done with Day 2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -2,8 +2,38 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day2a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const games = await readData(dataPath);
+
+  let sum = 0;
+  for (const game of games) {
+    const [gameLabel, allPairs] = game.split(':');
+    const gameId = parseInt(gameLabel.slice(5));
+    const pairs = allPairs.split(';').map((pair) =>
+      pair
+        .trim()
+        .split(',')
+        .map((ball) => {
+          const [number, type] = ball.trim().split(' ');
+          return { [type]: parseInt(number) };
+        })
+        .reduce((a, c) => {
+          return {
+            red: (a['red'] || 0) + (c['red'] || 0),
+            green: (a['green'] || 0) + (c['green'] || 0),
+            blue: (a['blue'] || 0) + (c['blue'] || 0),
+          };
+        }, {})
+    );
+    if (
+      pairs.some(
+        (pair) => pair['red'] > 12 || pair['green'] > 13 || pair['blue'] > 14
+      )
+    ) {
+      continue;
+    }
+    sum += gameId;
+  }
+  return sum;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -2,8 +2,46 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day2b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const games = await readData(dataPath);
+
+  let sum = 0;
+  for (const game of games) {
+    const [_, allPairs] = game.split(':');
+    const pairs = allPairs
+      .split(';')
+      .map((pair) =>
+        pair
+          .trim()
+          .split(',')
+          .map((ball) => {
+            const [number, type] = ball.trim().split(' ');
+            return { [type]: parseInt(number) };
+          })
+          .reduce((a, c) => {
+            return {
+              red: (a['red'] || 0) + (c['red'] || 0),
+              green: (a['green'] || 0) + (c['green'] || 0),
+              blue: (a['blue'] || 0) + (c['blue'] || 0),
+            };
+          }, {})
+      )
+      .reduce((a, c) => {
+        return {
+          red:
+            (a['red'] || 0) < (c['red'] || 0) ? c['red'] || 0 : a['red'] || 0,
+          green:
+            (a['green'] || 0) < (c['green'] || 0)
+              ? c['green'] || 0
+              : a['green'] || 0,
+          blue:
+            (a['blue'] || 0) < (c['blue'] || 0)
+              ? c['blue'] || 0
+              : a['blue'] || 0,
+        };
+      }, {});
+    sum += pairs['red'] * pairs['green'] * pairs['blue'];
+  }
+  return sum;
 }
 
 const answer = await day2b();

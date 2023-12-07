5fa41242a26463c243a11d9f52c714691f4a7f12
Ajit <ajitzero@gmail.com>
2023-12-08 04:47:33 +0530

    Done with Day 3

--- puzzles/day-3/day-3-a.ts
+++ puzzles/day-3/day-3-a.ts
@@ -1,9 +1,71 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const isSafe = (x: number, y: number, data: string[]) => {
+  const points = [
+    [
+      x - 1 >= 0 && y - 1 >= 0 ? data[x - 1][y - 1] : '.',
+      x - 1 >= 0 ? data[x - 1][y] : '.',
+      x - 1 >= 0 && y + 1 < data.length ? data[x - 1][y + 1] : '.',
+    ],
+    [
+      y - 1 >= 0 ? data[x][y - 1] : '.',
+      data[x][y],
+      y + 1 < data.length ? data[x][y + 1] : '.',
+    ],
+    [
+      x + 1 < data.length && y - 1 >= 0 ? data[x + 1][y - 1] : '.',
+      x + 1 < data.length ? data[x + 1][y] : '.',
+      x + 1 < data.length && y + 1 < data.length ? data[x + 1][y + 1] : '.',
+    ],
+  ];
+  // console.log(x, y, data.length, data[x][y], points);
+  return points
+    .flat(1)
+    .every((point) => point === '.' || !Number.isNaN(+point));
+};
+
 export async function day3a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sum = 0;
+  let prevWasNumber = false;
+  let unsafe = false;
+
+  for (let i = 0; i < data.length; ++i) {
+    const line = data[i];
+    let num = 0;
+    for (let j = 0; j < line.length; ++j) {
+      const spot = +line[j];
+      if (!Number.isNaN(spot)) {
+        num *= 10;
+        num += spot;
+        prevWasNumber = true;
+        if (!unsafe && !isSafe(i, j, data)) {
+          // console.log('mark not safe at', i, j, data[i][j], { num });
+          // num = 0;
+          unsafe = true;
+        }
+        continue;
+      }
+      if (prevWasNumber) {
+        if (unsafe) {
+          console.log('not safe', num);
+          sum += num;
+          unsafe = false;
+        }
+        num = 0;
+        prevWasNumber = false;
+      }
+    }
+    if (prevWasNumber && unsafe) {
+      console.log('not safe', num);
+      sum += num;
+      unsafe = false;
+    }
+    num = 0;
+    prevWasNumber = false;
+  }
+  return sum;
 }
 
 const answer = await day3a();
--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -1,9 +1,88 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Point = { x: number; y: number };
+
+const isNumber = (maybeNumber: string) => !Number.isNaN(+maybeNumber);
+
+const getCases = (point: Point, data: string[]) => {
+  const { x, y } = point;
+  const N = data.length;
+  const points = [
+    [
+      x - 1 >= 0 && y - 1 >= 0 && isNumber(data[x - 1][y - 1]) ? '1' : '0',
+      x - 1 >= 0 && isNumber(data[x - 1][y]) ? '1' : '0',
+      x - 1 >= 0 && y + 1 < N && isNumber(data[x - 1][y + 1]) ? '1' : '0',
+    ].join(''),
+    [
+      y - 1 >= 0 && isNumber(data[x][y - 1]) ? '1' : '0',
+      '0',
+      y + 1 < N && isNumber(data[x][y + 1]) ? '1' : '0',
+    ].join(''),
+    [
+      x + 1 < N && y - 1 >= 0 && isNumber(data[x + 1][y - 1]) ? '1' : '0',
+      x + 1 < N && isNumber(data[x + 1][y]) ? '1' : '0',
+      x + 1 < N && y + 1 < N && isNumber(data[x + 1][y + 1]) ? '1' : '0',
+    ].join(''),
+  ];
+  const cases: Point[] = [];
+  for (let i = -1; i < 2; ++i) {
+    if (['100', '110', '111'].includes(points[i + 1])) {
+      cases.push({ x: x + i, y: y - 1 });
+    }
+    if (['011', '010'].includes(points[i + 1])) {
+      cases.push({ x: x + i, y });
+    }
+    if (['001'].includes(points[i + 1])) {
+      cases.push({ x: x + i, y: y + 1 });
+    }
+    if (['101'].includes(points[i + 1])) {
+      cases.push({ x: x + i, y: y - 1 });
+      cases.push({ x: x + i, y: y + 1 });
+    }
+    if (cases.length > 2) break;
+  }
+  return cases;
+};
+
+const findNumber = (point: Point, data: string[]) => {
+  const { x, y } = point;
+
+  let left = y;
+  while (left >= 0 && isNumber(data[x][left])) {
+    --left;
+  }
+  if (left < 0) {
+    left = 0;
+  } else if (!isNumber(data[x][left])) {
+    ++left;
+  }
+
+  let num = 0;
+  while (left < data[x].length && isNumber(data[x][left])) {
+    num *= 10;
+    num += +data[x][left];
+    ++left;
+  }
+  console.log(point, data[x][y], num);
+  return num;
+};
+
 export async function day3b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sum = 0;
+  for (let x = 0; x < data.length; ++x) {
+    for (let y = 0; y < data[x].length; ++y) {
+      if (data[x][y] === '*') {
+        const cases = getCases({ x, y }, data);
+        if (cases.length === 2) {
+          sum += findNumber(cases[0], data) * findNumber(cases[1], data);
+          console.log('---');
+        }
+      }
+    }
+  }
+  return sum;
 }
 
 const answer = await day3b();
